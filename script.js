6c81d6a0e630fa3d48dcc023b118dfbf3e1a00b7
Eduardo <eduarduholiveira@gmail.com>
2022-07-19 13:40:21 -0300

    turma-barbosa-a

---
+++ script.js
@@ -0,0 +1,39 @@
+ ola mundo 
+se usar barra asteriscos fazemos comentarios de bloco */
+ console.log(prompt("qual seu nome ?"))
+
+
+let idade = prompt("qual a sua idade ?")
+
+
+
+ 
+ let temPet= true
+ const nomePet = "Luke"
+ console.log(nomePet)
+ 
+ const idadeNumber = Number(idade)
+ const idadeString = idade.toString()
+
+ console.log(typeof temPet)
+ console.log(typeof nomePet)
+ console.log(typeof idadeString)*/
+
+ const Celular = "samsung"
+let contato = 997070
+let temCel= true
+
+console.log(typeof Celular)
+console.log(typeof contato)
+console.log(typeof temCel)
+
+let A = 20
+let B = 30
+let C;
+
+C=A
+A=B
+B=C
+
+console.log(A)
+console.log(B)
