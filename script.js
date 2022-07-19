f58e2d85326db451d95db9544e425394789f40f3
Eduardo <eduarduholiveira@gmail.com>
2022-07-19 13:59:48 -0300

    exercicios a07 Barbosa A

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- ola mundo 
-se usar barra asteriscos fazemos comentarios de bloco */
+/* ola mundo 
+se usar barra asteriscos fazemos comentarios de bloco 
  console.log(prompt("qual seu nome ?"))
 
 
@@ -17,7 +17,7 @@ let idade = prompt("qual a sua idade ?")
 
  console.log(typeof temPet)
  console.log(typeof nomePet)
- console.log(typeof idadeString)*/
+ console.log(typeof idadeString)
 
  const Celular = "samsung"
 let contato = 997070
@@ -36,4 +36,30 @@ A=B
 B=C
 
 console.log(A)
-console.log(B)
+console.log(B)*/
+// const nome = "Eduardo Henrique de Oliviera"
+
+// let idade = 29
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+const nome  = prompt("Qual a seu nome ?")
+
+let idade = prompt("Qual a sua idade ?")
+
+console.log(nome,idade)
+
+const pergunta1 = prompt("vc esta usando roupa azul?")
+const pergunta2 = prompt ("você esta gosta de beattles ?")
+const pergunta3 = prompt("você é religioso?")
+
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
+
+let a=10
+let b=25
+let c;
+
+c=a
+b=c
